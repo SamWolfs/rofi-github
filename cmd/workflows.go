0c4d8e05a88765f4a5b1e847604b410fc8d36c54
Sam Wolfs <46925128+SamWolfs@users.noreply.github.com>
2023-03-05 18:02:58 +0100

    Add workflows resource and update implementation (#3)

--- cmd/workflows.go
+++ cmd/workflows.go
@@ -22,6 +22,7 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"os"
@@ -33,32 +34,35 @@ import (
 )
 
 func init() {
-	rootCmd.AddCommand(repositoriesCmd)
+	rootCmd.AddCommand(workflowsCommand)
 }
 
-var repositoriesCmd = &cobra.Command{
-	Use:   "repositories",
-	Short: "List all user-owned and user-configured repositories.",
-	Long: `List all user-owned and user-configured repositories.
-By default only user-owned repositories will be returned;
-to add repositories owned by other users, add them to your configuration file.`,
+var workflowsCommand = &cobra.Command{
+	Use: "workflows",
+	// TODO: descriptions
+	Short: "List all user-owned and user-configured workflows.",
+	Long: `List all user-owned and user-configured workflows.
+By default only user-owned workflows will be returned;
+to add workflows owned by other users, add them to your configuration file.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		if len(args) == 0 {
-			var repositories []Repository
-			mapstructure.Decode(metadata.Get("repositories"), &repositories)
-			rows := make([]string, len(repositories))
-			for i, repo := range repositories {
-				// Provide the url as "hidden" info, available from the $ROFI_INFO environment variable
-				rows[i] = repo.Name + "\x00info\x1f" + repo.Url
+			var workflows []Workflow
+			mapstructure.Decode(metadata.Get("workflows"), &workflows)
+			rows := make([]string, len(workflows))
+			for i, workflow := range workflows {
+				info, _ := json.Marshal(workflow)
+				rows[i] = workflow.Name + "\x00info\x1f" + string(info[:])
 			}
 			fmt.Print(strings.Join(rows, "\n"))
 		} else {
-			url := os.Getenv("ROFI_INFO")
-			if url == "" {
-				log.Fatal("Choose an repository.")
+			response := os.Getenv("ROFI_INFO")
+			if response == "" {
+				log.Fatal("Choose a workflow.")
 				return
 			}
-			_, stdErr, err := gh.Exec("repo", "view", "--web", url)
+			var workflow Workflow
+			json.Unmarshal([]byte(response), &workflow)
+			_, stdErr, err := gh.Exec("workflow", "view", "-R", workflow.Repository, "--web", workflow.Id)
 			if err != nil {
 				log.Fatal(string(stdErr.Bytes()[:]))
 			}
